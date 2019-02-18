d737d18d80f195b0e05845cde785b03913b8f94e
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2019-02-18 08:35:58 +0100

    Update yaml.py
    First example

--- yaml.py
+++ yaml.py
@@ -1,1 +1,13 @@
+import yaml
 
+# open reading
+try:                   
+    with open('my_yaml.yml', 'r') as my_yaml:
+        my_yaml_reader = yaml.load(my_yaml)
+        variable = my_yaml_reader[1]
+        another_variable = my_yaml_reader[4]
+except:
+    print("Could not read yaml file, please create proper configuration!")
+    
+# Refresh general file by opening in write mode    
+open(logFilePath,"w+")
