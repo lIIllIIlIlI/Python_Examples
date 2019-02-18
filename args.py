f3c9ce26a55750a65fc72cb3c4bea69f6c197bcc
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2019-02-18 08:39:45 +0100

    Update args.py
    added basic example

--- args.py
+++ args.py
@@ -1,1 +1,12 @@
+import argparse
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--clean','-c', help = 'clean build folder before compilation', action="store_true", default=False)
+parser.add_argument('--instrumentation','-i', help = 'List of instrumentations, multiple instrumentations allowed. Example: --instrumentation Xcp Profiler', choices = build_instrumentations, nargs = '+', required = True)
+parser.add_argument('--nocores', '-n', help='Number of vCores available in the system is default.', type = int, required = False)
+parser.add_argument('--release','-r', help='Enables deterministic build for release', action="store_true", default=False)
+
+args = parser.parse_args()
+
+print(args.nocores)
