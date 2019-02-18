7bd637db239a20c8e9c93ee3b142327c58716136
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2019-02-18 13:50:05 +0100

    Update cmdCall.py

--- cmdCall.py
+++ cmdCall.py
@@ -1,1 +1,13 @@
-
+import subprocess
+ 
+  command = ["hello","world"]
+  process = subprocess.Popen(command, bufsize=1, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            for line in iter(process.stdout.readline, ''):  
+                runLogger.info(line.rstrip())
+            process.wait()
+            if process.returncode:
+                logString = "FAIL"
+                resultLogger.error(logString)
+            else:
+                logString = "SUCCESS"
+                resultLogger.info(logString)
