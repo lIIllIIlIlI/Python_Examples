a9e400ac0ac2af011ac3f4a7f00155f8eebe1053
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2019-07-12 11:46:16 +0200

    Update FileIO.py

--- FileIO.py
+++ FileIO.py
@@ -13,7 +13,7 @@
 
 
 import yaml
-
+# YAML --------------------------------------------------------------------------
 # open reading
 try:                   
     with open('my_yaml.yml', 'r') as my_yaml:
@@ -25,3 +25,60 @@ except:
     
 # Refresh general file by opening in write mode    
 open(logFilePath,"w+")
+-------------------------------------------------------------------------------------
+
+
+# CSV -------------------------------------------------------------------------------
+
+ elfElementList = listFiles(elfPath, extension)
+    for elfElement in elfElementList:
+        addColumnCsvFile(elfElement, currentTime)
+
+    print("\n\n\n")
+    with open(currentTime + ".prof", 'r', newline='') as csvfile:
+        memReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in memReader:
+            print("{:<20}{:>10}{:>10}".format(*row))
+            try:
+                ramUsageSum += int(row[1])
+                romUsageSum += int(row[2])
+            except:
+                pass
+            
+  with open(currentTime + ".prof", 'a', newline='') as csvfile:
+        memWriter = csv.writer(csvfile, delimiter=' ',
+                               quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        memWriter.writerow(['Sum', ramUsageSum, romUsageSum])
+     
+            
+ def initCsvFile():
+    """
+    Deletes all values of temporary excel file
+    """
+    global currentTime
+    # Create profile
+    tmpFd = open(currentTime + ".prof", "w+")
+    tmpFd.close()
+
+    # Add Metadata to profile
+    addMetadataToCsvFile()
+
+    try:
+        with open(currentTime + ".prof", 'a', newline='') as csvfile:
+            memWriter = csv.writer(
+                csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            memWriter.writerow(['Element', 'Ram', 'Rom'])
+    except:
+        memLogger.error("Unable to refresh profile. Resulting profile might be corrupted."
+                        "Continuing ...")
+
+   
+
+with open(currentTime + ".prof", 'a', newline='') as csvfile:
+                memWriter = csv.writer(csvfile, delimiter=' ',
+                                       quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                memWriter.writerow(['Version control', 'no', ''])
+                memWriter.writerow(['Folder name', elfPath, ''])
+                memWriter.writerow(['', '', ''])
+
+# ------------------------------------------------------------------------------------
