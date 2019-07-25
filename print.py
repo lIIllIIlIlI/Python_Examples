755c2afc3cbb3bcbb0a86872e835f0c4f86f5f8e
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2019-07-25 10:23:55 +0200

    Update print.py

--- print.py
+++ print.py
@@ -1,15 +1,1 @@
-import pprint
-
-def namestr(obj, namespace):
-    return [name for name in namespace if namespace[name] is obj]
-
-def advancedPrint(printElement):
-   print(namestr(printElement, globals()))
-   pprint.pprint(printElement, width=10) 
-    
-myDict = {"variable": "5",
-          "list": [1,2,3,4,5],
-          "stringList": ["eins", "zwei", "drei", "vier", "fünf"],
-          }
-
-advancedPrint(myDict)
+print("{:<20}{:>10}{:>10}".format(*row))
