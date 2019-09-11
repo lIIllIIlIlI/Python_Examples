6a21c09df50daa7020bd62675fbd64da6da3340d
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2019-09-11 15:34:40 +0200

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -6,35 +6,27 @@ class A:
     self.name = name
     self.myString = myString
     self.anotherVar = 5
-    
-#   def __repr__(self):
-#     """
-#     There is a implicit default __repr__ that prints "self.__class__.__name__, id(self)". This defines what is returned 
-#     when print is called on the function. To create a pretty printing, this can be explicitely overriden.
-#     """
-#     return "<class: %s\n name:%s,\n myString:%s,\n anotherVar:%d>" % \
-#          (self.__class__.__name__, \
-#           self.name, self.myString, \
-#               self.anotherVar)
 
-  def __repr__(self):
-    """
-    There is a implicit default __repr__ that prints "self.__class__.__name__, id(self)". This defines what is returned 
-    when print is called on the function. To create a pretty printing, this can be explicitely overriden.
-    __repr__ should give some information about the object to developer.
-    """
-    returnString = ""
-    returnString += "<class: " + self.__class__.__name__ + ",\n"
-    returnString += " name: " + str(self.name) + ",\n"
-    returnString += " myString: " + str(self.myString) + ",\n"
-    returnString += " anotherVar: " + str(self.anotherVar) + ">\n"
-    return returnString
-    
   def __str__(self):
+    """
+    This one should give the user a quick feedback
+    """
+    objectDescriptionString = "\n"
+    objectDescriptionString += " name: " + str(self.name) + ",\n"
+    objectDescriptionString += " myString: " + str(self.myString) + ",\n"
+    objectDescriptionString += " anotherVar: " + str(self.anotherVar) + ">\n"
+    return objectDescriptionString
+    
+  def __repr__(self):
       """
-      This one should give the user a quick feedback.
+      There is a implicit default __repr__ that prints "self.__class__.__name__, id(self)". This defines what is returned 
+      when print is called on the function. To create a pretty printing, this can be explicitely overriden.
+      __repr__ should give some information about the object to developer.
       """
-      return "Im here"
-
+      objectDescriptionString = "\n"
+      objectDescriptionString += "<class: " + self.__class__.__name__ + ",\n"
+      objectDescriptionString += " name: " + str(self.name) + ",\n"
+      return objectDescriptionString
+      
 myObject = A("greeting", "hello world") 
 print(myObject)
