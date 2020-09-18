0b1301a693ff5a48f21851c4c301e45781d6f720
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2020-09-18 12:58:18 +0200

    Update intBaseConversion.py

--- intBaseConversion.py
+++ intBaseConversion.py
@@ -16,6 +16,29 @@ myAlphaHex = int(myAlphaHexString,16)
 mergedHex = myHex + myAlphaHex + myOffset
 print("Offset added to hex: {}. New hex number:{}".format(mergedHex, hex(mergedHex)))
 
+def getBitMask(offset, bitMaskWidth, dataTypeSize):
+    """
+    Generates and returns a hexadecimal bit mask that is aligned to the data type size via
+    padding.
+
+    @offset: desired shift of the bitmask
+    @bitMaskWidth: number of bits for the bitmask
+    """
+    bitWidth = dataTypeSize * 8
+    hexDigitNumber = int(bitWidth / 4)
+    rawBitMask = ""
+    if offset + bitMaskWidth > bitWidth:
+        logger.warning("Overflow in BitMask converter. The sum of the Offset %s and \
+the bit mask width %s exceeds the given width of %s bit", offset, bitMaskWidth, bitWidth)
+    for bitPosition in range(bitWidth):
+        if bitPosition >= offset and bitPosition < offset + bitMaskWidth:
+            rawBitMask += "1"
+        else:
+            rawBitMask += "0"
+    bitMask = hex(int(rawBitMask, 2))
+    bitMask = insertHexPadding(bitMask, hexDigitNumber)
+    return bitMask
+
 def insertHexPadding(hexString, paddingWidth):
     """
     Pads the given hexadecimal string with zeros to reach the desired paddingWidth in lenght.
