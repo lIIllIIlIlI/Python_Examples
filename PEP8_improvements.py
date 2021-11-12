9e0622a955bb3177c54a18d250d7e40d86978879
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2021-11-12 18:49:47 +0100

    Create PEP8_improvements.py

---
+++ PEP8_improvements.py
@@ -0,0 +1,5 @@
+# 1. GLOBAL_VARIABLES always upper case
+# 2. specify input/out types whenever possible 'def calc_volume(x: int, y: int, z: int) -> int:', even if return type is None
+# 3. Use enums instead of strings! Have a look at auto.
+# 4. Unit von variablen hinzufügen, e.g. Byte, dollar
+# 5. 
